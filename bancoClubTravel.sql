1c76cec87912183ba5f79402b53b0fb1116cdf5e
danielleit <81180528+danielleit@users.noreply.github.com>
2023-02-02 21:47:43 -0300

    add tables sub_news user_permission users permission contact

--- bancoClubTravel.sql
+++ bancoClubTravel.sql
@@ -19,64 +19,97 @@ create table region(
 
 create table state(
  id_state bigint primary key auto_increment,
- region bigint,
- country bigint,
  uf_code varchar(100) unique,
  name varchar(100),
- uf varchar(100)
+ uf varchar(100),
+ region bigint,
+ country bigint
 );
 
 create table city(
  id_city bigint primary key auto_increment,
- state bigint,
  ibge_code varchar(100),
- name varchar(100)
+ name varchar(100),
+ state bigint
 );
 
 create table destinations(
  id_destinations bigint primary key auto_increment,
- city bigint,
  reference varchar(100),
  image_link varchar(300),
- image mediumblob
+ image mediumblob,
+ city bigint
 );
 
 create table traveler(
  id_traveler bigint primary key auto_increment,
+ image_link varchar(300),
+ image mediumblob,
+ contact bigint,
+ users bigint
+);
+
+create table contact(
+ id_contact bigint primary key auto_increment,
  name varchar(100),
  phone varchar(13),
- email varchar(100) unique,
- image_link varchar(300),
- image mediumblob
+ email varchar(100) unique
+);
+
+create table users(
+ id_user bigint primary key auto_increment,
+ user_name varchar(100) unique,
+ password varchar(10)
+);
+
+create table permission(
+ id_permission bigint primary key auto_increment,
+ description varchar(300)
 );
 
 create table news(
  id_news bigint primary key auto_increment,
- traveler bigint,
- destinations bigint,
  title varchar(100),
- subtitle varchar(100),
  text varchar(300),
  date_time datetime,
  image_link varchar(300),
- image mediumblob
+ image mediumblob,
+ traveler bigint,
+ destinations bigint
+);
+
+create table sub_news(
+ id_sub_news bigint primary key auto_increment,
+ sub_title varchar(100),
+ text varchar(100),
+ image_link varchar(300),
+ image mediumblob,
+ news bigint
 );
 
 create table comments(
  id_comments bigint primary key auto_increment,
- traveler bigint,
- news bigint,
  text varchar(300),
- date_time datetime
+ date_time datetime,
+ traveler bigint,
+ news bigint
 );
 
 create table events(
  id_events bigint primary key auto_increment,
- destinations bigint,
  start_date datetime,
  end_date datetime,
- description varchar(200)
+ description varchar(200),
+ destinations bigint
 );
+
+create table user_permission(
+ id_user bigint,
+ id_permission bigint,
+ primary key(id_user, id_permission),
+ users bigint,
+ permission bigint
+ );
 
 alter table state add constraint fk_state_region foreign key (region) references region(id_region);
 alter table state add constraint fk_state_country foreign key (country) references country(id_country);
@@ -87,3 +120,8 @@ alter table news add constraint fk_news_destinations foreign key (destinations) 
 alter table comments add constraint fk_comments_traveler foreign key (traveler) references traveler(id_traveler);
 alter table comments add constraint fk_comments_news foreign key (news) references news(id_news);
 alter table events add constraint fk_events_destinations foreign key (destinations) references destinations(id_destinations);
+alter table traveler add constraint fk_traveler_contact foreign key (contact) references contact(id_contact);
+alter table traveler add constraint fk_traveler_users foreign key (users) references users(id_user);
+alter table user_permission add constraint fk_up_users foreign key (users) references users(id_user);
+alter table user_permission add constraint fk_up_permission foreign key (permission) references permission(id_permission); 
+alter table sub_news add constraint fk_sn_news foreign key (news) references news(id_news);
